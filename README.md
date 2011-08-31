83f4e14a1c7121aa4f6c990ca8bcd5085c467334
Lukas Knuth <mr.luke.187@googlemail.com>
2011-08-31 21:08:02 +0200

    Linked the wiki in the Readme.

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ A Java library which is used to check for updates (newer versions) of a software
  hosted on sites like GitHub or SourceForge.
  
 ## Features ##
-The library comes with Providers for GitHub and SourceForge, own Providers might
- be created by the user.
+The library comes with a few simple Providers, own Providers might
+ be created by the user, too.
  
 The library also offers two ways of checking if there is a newer version of the
  software available:
@@ -13,15 +13,20 @@ The library also offers two ways of checking if there is a newer version of the
  * By using a "Date"-Object
  * By using the librarys "Version"-Object
  
- Both those ways are documented in the Wiki of this project. There are also Wiki-
- entry's for the standard Providers:
- 
- * `GitHubProvider`
- * `SourceForgeProvider`
- 
-A little tutorial on how to create own Providers might also be found here.
+Both those ways are 
+ [documented in the Wiki](https://github.com/LukasKnuth/chkUpdate/wiki/Basic-How-To)
+ of this project. There are also Wiki-entry's for the 
+ [standard Providers](https://github.com/LukasKnuth/chkUpdate/wiki/Standard-Providers).
 
-## How to use ##
+A little tutorial on how to 
+ [create own Providers](https://github.com/LukasKnuth/chkUpdate/wiki/Create-your-own-Provider)
+ might also be found in the Wiki.
+
+### Used projects ###
+This library uses [JSON-java](https://github.com/douglascrockford/JSON-java) by
+ [Douglas Crockford](https://github.com/douglascrockford) to parse JSON-Strings.
+
+## How to build ##
 The library ships with SourceCode and Documentation only!
 
 The provided Ant-buildscript can be used to create a `.jar`-file for easy integration
--- src/test/Test.java
+++ src/test/Test.java
@@ -16,23 +16,44 @@ public class Test {
 	public Test(){
 		System.out.println("Check GitHub");
 		github();
+		System.out.println("Check GitHub (Date)");
+		githubDate();
 		System.out.println("-----\nCheck SourceForge");
 		sourceforge();
 	}
 	
 	private void github(){
-		Version v = new Version("v0.1");
+		Version v = new Version("v0.1"); // The Local Version.
 		GitHubProvider gh = new GitHubProvider("LukasKnuth", "NoteBook");
 		UpdateCheck check = new UpdateCheck(gh);
 		try {
 			UpdateResult res = check.checkForUpdates();
-			System.out.println("Lokal: "+v.toString()+"|Remote: "+res.getVersion().toString());
-			System.out.println("Braucht Update: "+res.getVersion().isNewerThen(v));
+			System.out.println("Local: "+v.toString()+
+					"|Remote: "+res.getVersion().toString()
+			);
+			System.out.println("Needs Update: "+
+					res.getVersion().isNewerThen(v)
+			);
 		} catch (UpdateRequestException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UpdateResponseException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void githubDate(){
+		Date local = new Date((2007 - 1900), 12, 12); // Local Version
+		Provider gh = new GitHubProvider("LukasKnuth", "NoteBook");
+		UpdateCheck chk = new UpdateCheck(gh);
+		try {
+			UpdateResult res = chk.checkForUpdates();
+			System.out.println("Local: "+local.toString()+
+					"|Remote: "+res.getUpdateDate().toString());
+			System.out.println("Needs update: "+
+					res.getUpdateDate().after(local));
+		} catch (UpdateRequestException e) {
+			e.printStackTrace();
+		} catch (UpdateResponseException e) {
 			e.printStackTrace();
 		}
 	}
@@ -44,17 +65,15 @@ public class Test {
 		UpdateCheck chk = new UpdateCheck(sf);
 		try {
 			UpdateResult res = chk.checkForUpdates();
-			System.out.println("Lokal: "+current.toLocaleString()+
-					"|Remote: "+res.getUpdateDate().toLocaleString());
+			System.out.println("Lokal: "+current.toString()+
+					"|Remote: "+res.getUpdateDate().toString());
 			System.out.print("Braucht update: ");
 			if (res.getUpdateDate().after(current)){
 				System.out.print("true");
 			} else System.out.print("false"); 
 		} catch (UpdateRequestException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UpdateResponseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
